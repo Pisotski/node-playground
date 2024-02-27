ad45cc8155184212fe186101998dd6cf8e70fb81
Volodymyr Pisotskyi <volodymyr.pisotskyi@volodymyrs-mbp.lan>
2024-02-27 12:44:00 -0800

    first commit, first week completed

---
+++ 11-fs-async.js
@@ -0,0 +1,18 @@
+const {readFile, writeFile} = require('fs')
+
+console.log('start')
+const callback = (err, result) => {
+    if(err) return
+    const first = result
+    readFile('./content/second.txt', 'utf8', (err,result) => {
+        if(err) return
+        const second = result
+        writeFile('./content/result-async.txt', `Async merge: ${first}, ${second} `, 'utf8', (err,result) => {
+            if(err) return
+            console.log('done with this task')
+        })
+    } )
+}
+readFile('./content/first.txt', 'utf8', callback )
+
+console.log('starting next file')
---
+++ 4-names.js
@@ -0,0 +1,10 @@
+// local
+const secret = 'SUPER SECRET'
+// share
+const john = 'john'
+const peter = 'peter'
+
+// every file in node has module properties!
+// console.log(module)
+
+module.exports = {john, peter}
